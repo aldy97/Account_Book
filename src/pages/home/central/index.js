9b927861f06aca099c9c615fe9d0c664d683a6ec
Feng Xiong <fengxiong34@gmail.com>
2020-06-27 13:01:29 -0400

    modified central

--- src/pages/home/central/index.js
+++ src/pages/home/central/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import theme from '../../../static/theme';
 
 function Central() {
   const StyledCentral = styled.div`
+    height: 1000px;
+    width: ${theme.$width};
     z-index: 0;
     margin-top: 200px;
-    height: 1000px;
     background: #eee;
+    @media (max-width: 500px) {
+      width: calc(100vw);
+    }
   `;
   return <StyledCentral>Hello World</StyledCentral>;
 }
